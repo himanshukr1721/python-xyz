7197f3aa2abdfc3efbbf8fd226e245035b18f8f9
Himanshu Kumar <himanshukumar236@lpu.in>
2025-08-03 23:04:56 +0530

    hi

--- 00_python/non_python_code.py
+++ 00_python/non_python_code.py
@@ -1,1 +1,13 @@
-print("hello world")
+class Chai:
+    def __init__(self, sweetness, milk_level):
+        self.sweetness = sweetness
+        self.milk_level = milk_level
+    
+    def sip(self):
+        print("taking a sip")
+    
+    def add_sugar(self, amount):
+        print("added the sugar")
+
+my_chai = Chai(sweetness=3, milk_level=2)
+
